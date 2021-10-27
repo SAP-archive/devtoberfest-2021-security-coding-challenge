1a4d818ef04b7e4db2a8e0649ddd23ea0af1f897
Sergei Haller <sergei.haller@u-niq.com>
2021-10-27 20:20:52 +0200

    add CSP

--- cap/srv/server.js
+++ cap/srv/server.js
@@ -1,5 +1,6 @@
 const cds = require('@sap/cds')
 const cors = require("cors")
+const helmet = require("helmet")
 
 cds.on('bootstrap', async (app) => {
 
@@ -7,6 +8,16 @@ cds.on('bootstrap', async (app) => {
     // see https://www.npmjs.com/package/cors#configuration-options
     // for available options
     app.use(cors()) 
+
+    // add CSP
+    app.use(helmet({
+        contentSecurityPolicy: {
+          directives: {
+            ...helmet.contentSecurityPolicy.getDefaultDirectives(),
+            // custom settings
+          }
+        }
+      }))
 })
 
 module.exports = cds.server

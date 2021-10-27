14f120054621d3726528f2d79c756cb6bd66535a
Sergei Haller <sergei.haller@u-niq.com>
2021-10-27 20:06:24 +0200

    add CORS

--- cap/srv/server.js
+++ cap/srv/server.js
@@ -1,6 +1,12 @@
-const cds = require ('@sap/cds')
-cds.on('bootstrap', (app) => {
+const cds = require('@sap/cds')
+const cors = require("cors")
 
+cds.on('bootstrap', async (app) => {
+
+    // just allow any origin...
+    // see https://www.npmjs.com/package/cors#configuration-options
+    // for available options
+    app.use(cors()) 
 })
 
 module.exports = cds.server

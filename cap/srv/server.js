809c5362324e126d073003f18a8a34911e981127
Enric Castella <ecastella@outlook.com>
2021-10-28 20:40:12 +0000

    Add A Content Security Policy

--- cap/srv/server.js
+++ cap/srv/server.js
@@ -1,9 +1,20 @@
-const cds = require ('@sap/cds')
-const cors = require('cors')
+const cds = require("@sap/cds");
+const cors = require("cors");
+const helmet = require("helmet");
 
-cds.on('bootstrap', (app) => {
-    //cors security
-    app.use(cors())
-})
+cds.on("bootstrap", (app) => {
+  //cors security
+  app.use(cors());
+  app.use(
+    helmet({
+      contentSecurityPolicy: {
+        directives: {
+          ...helmet.contentSecurityPolicy.getDefaultDirectives(),
+          // custom settings
+        },
+      },
+    })
+  );
+});
 
-module.exports = cds.server
+module.exports = cds.server;

76cf34bbe56ac98d4809068fa65af010a2c73b9f
Laurens Deprost <Laurens.Deprost@Laurix.com>
2021-10-28 20:27:38 +0200

    fix: Fix cap issues

--- cap/srv/server.js
+++ cap/srv/server.js
@@ -1,6 +1,27 @@
-const cds = require ('@sap/cds')
+const cds = require('@sap/cds');
+const cors = require('cors');
+const helmet = require('helmet');
+
 cds.on('bootstrap', (app) => {
+    // enable CORS in unproductive environments
+    if (process.env.NODE_ENV !== 'production') {
+        const options = {
+            origin: 'http://localhost:4004',
+            optionsSuccessStatus: 200,
+            credentials: true,
+        };
+        app.use(cors(options));
+    }
 
-})
+    app.use(
+        helmet({
+            contentSecurityPolicy: {
+                directives: {
+                    ...helmet.contentSecurityPolicy.getDefaultDirectives(),
+                },
+            },
+        })
+    );
+});
 
-module.exports = cds.server
+module.exports = cds.server;

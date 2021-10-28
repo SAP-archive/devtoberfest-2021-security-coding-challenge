3a0f9f09e5ab642b6be7df0e175aac99446b2048
Enric Castella <e_castella@hotmail.com>
2021-10-28 21:23:52 +0000

    CAPm challenge

--- cap/srv/server.js
+++ cap/srv/server.js
@@ -1,6 +1,21 @@
-const cds = require ('@sap/cds')
-cds.on('bootstrap', (app) => {
+const cds = require("@sap/cds");
+const cors = require("cors");
+const helmet = require("helmet");
 
-})
+cds.on("bootstrap", (app) => {
+  //cors security
+  app.use(cors());
+  app.use(
+    helmet({
+      contentSecurityPolicy: {
+        directives: {
+          ...helmet.contentSecurityPolicy.getDefaultDirectives(),
+          // custom settings
+        },
+      },
+    })
+  );
+});
 
-module.exports = cds.server
+module.exports = cds.server;
+

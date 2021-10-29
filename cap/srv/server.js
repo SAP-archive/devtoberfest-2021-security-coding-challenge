a98b4b994b71d0f373b04a51cc37eb9df11f4b8f
ag3silaus <burakyilmaz778@gmail.com>
2021-10-29 11:20:41 +0200

    just_cap

--- cap/srv/server.js
+++ cap/srv/server.js
@@ -1,6 +1,19 @@
-const cds = require ('@sap/cds')
+const cds = require("@sap/cds");
+const cors = require("cors");
+const helmet = require("helmet");
 cds.on('bootstrap', (app) => {
-
+    
+    app.use(cors())
+    app.use(
+        helmet({
+          contentSecurityPolicy: {
+            directives: {
+              ...helmet.contentSecurityPolicy.getDefaultDirectives(),
+            },
+          },
+        })
+      );
+   
 })
 
 module.exports = cds.server

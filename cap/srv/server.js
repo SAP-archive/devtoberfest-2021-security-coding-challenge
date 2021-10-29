5ea90f6d9f6dd4e30e246aa28694fdc1d4c8b867
schmelto <30869493+schmelto@users.noreply.github.com>
2021-10-29 08:44:34 +0200

    add cors

--- cap/srv/server.js
+++ cap/srv/server.js
@@ -1,6 +1,21 @@
 const cds = require ('@sap/cds')
-cds.on('bootstrap', (app) => {
+const cors = require ('cors')
+const helmet = require ('helmet')
 
-})
+cds.on('bootstrap', (app) => {
+    // cors seccurity
+    app.use(cors())
+
+    // helmet seccurity
+    app.use(
+        helmet({
+            contentSecurityPolicy: {
+                directives: {
+                    ...helmet.contentSecurityPolicy.getDefaultDirectives(),
+                },
+            },
+        }),
+    );
+});
 
 module.exports = cds.server

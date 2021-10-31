d4de4a6bf119f91bf85b48489263c3969f3f2af5
jyothiraditya.k <aditya.jyothir@gmail.com>
2021-10-31 19:19:33 +0530

    WEEK4CHALLENGE jyothiraditya.k

--- cap/srv/server.js
+++ cap/srv/server.js
@@ -4,11 +4,11 @@ cds.on('bootstrap', (app) => {
     var cors = require('cors')
     var helmet = require('helmet')
 
-
     app.use(cors())
 
     console.log('In the bootstrap')
 
     app.use(helmet())
 })
+
 module.exports = cds.server

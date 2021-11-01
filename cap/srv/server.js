bd4cebe6b45aa059a9d4368ed757acdf27b1edd4
Amaury Van Espen <AmauryVanEspen@users.noreply.github.com>
2021-11-01 13:34:10 +0100

    Update server.js
    Add usage of helmet

--- cap/srv/server.js
+++ cap/srv/server.js
@@ -1,7 +1,13 @@
-const cds = require ('@sap/cds')
+const cds = require('@sap/cds')
 cds.on('bootstrap', (app) => {
     const cors = require('cors')
     app.use(cors())
+    
+    var helmet = require('helmet')
+
+    console.log('In the bootstrap')
+
+    app.use(helmet())
 })
 
 module.exports = cds.server

8d0c710c371e1577aa8fcaeee48bd4fd31bc4fad
mozhgan-ghasemzadeh <mojgan.ghasemzade@gmail.com>
2021-11-01 17:44:27 +0000

    WEEK4CHALLENGE

--- cap/srv/server.js
+++ cap/srv/server.js
@@ -1,6 +1,16 @@
 const cds = require ('@sap/cds')
 cds.on('bootstrap', (app) => {
+    const cors = require('cors')
+    const helmet = require('helmet')
 
+    app.use(cors())
+    app.use(helmet({
+        contentSecurityPolicy: {
+          directives: {
+            ...helmet.contentSecurityPolicy.getDefaultDirectives(),
+          }
+        }
+    }))
 })
 
 module.exports = cds.server

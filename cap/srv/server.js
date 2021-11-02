8ba02f5a4c50ae8a9bdcfd3e41489e74b4cc253b
Jacek W <wozjac@zoho.com>
2021-11-02 22:48:05 +0100

    CAP security changes

--- cap/srv/server.js
+++ cap/srv/server.js
@@ -1,6 +1,22 @@
-const cds = require ('@sap/cds')
-cds.on('bootstrap', (app) => {
+const cds = require("@sap/cds");
+const helmet = require("helmet");
+const cors = require("cors");
 
-})
+cds.on("bootstrap", (app) => {
+  if (process.env.NODE_ENV !== "production") {
+    app.use(cors()); // skiping more configuration for simplicity
+  }
 
-module.exports = cds.server
+  app.use(
+    helmet({
+      contentSecurityPolicy: {
+        directives: {
+          ...helmet.contentSecurityPolicy.getDefaultDirectives(),
+          // some custom ones
+        },
+      },
+    })
+  );
+});
+
+module.exports = cds.server;
